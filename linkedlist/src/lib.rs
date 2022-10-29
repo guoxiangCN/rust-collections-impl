01e4f5a8189a4b314b8207216c228714abab7fe4
kuiper <mzygdeaq@163.com>
2022-10-29 23:23:37 +0800

    feat: pop_front

--- linkedlist/src/lib.rs
+++ linkedlist/src/lib.rs
@@ -18,10 +18,12 @@ impl<T> LinkedList<T> {
         }
     }
 
+    #[inline]
     pub fn length(&self) -> usize {
         self.length
     }
 
+    #[inline]
     pub fn is_empty(&self) -> bool {
         self.length == 0
     }
@@ -61,24 +63,67 @@ impl<T> LinkedList<T> {
 
     pub fn pop_front(&mut self) -> Option<T> {
         match self.head {
-            None => return None,
+            None => {
+                assert_eq!(self.length, 0);
+                return None;
+            }
             Some(ref mut h) => {
                 let next = h.next.take();
                 let save_head = self.head.take();
                 self.head = next;
                 self.length -= 1;
-                return save_head.map(|x| x.data);
+                return save_head.map(|x| (*x).data);
             }
         }
     }
 
     pub fn pop_back(&mut self) -> Option<T> {
+        match self.head {
+            None => {
+                assert_eq!(self.length, 0);
+                return None;
+            }
+            Some(ref mut x) => {
+                todo!()
+            }
+        }
         None
+    }
+
+    pub fn get(&self, index: usize) -> Option<&T> {
+        if index >= self.length {
+            return None;
+        }
+        match self.head {
+            None => {
+                assert_eq!(0, self.length);
+                return None;
+            }
+            Some(_) => {
+                let mut idx = index;
+                let mut cur: &Option<Box<ListNode<T>>> = &self.head;
+                loop {
+                    match cur {
+                        None => {
+                            return None;
+                        }
+                        Some(ref x) => {
+                            cur = &x.next;
+                            if idx == 0 {
+                                return Some(&x.data);
+                            }
+                            idx -= 1;
+                        }
+                    }
+                }
+            }
+        }
     }
 }
 
 #[cfg(test)]
 mod tests {
+
     use super::*;
 
     #[test]
@@ -90,7 +135,16 @@ mod tests {
         list.push_front(3);
         list.push_front(2);
         list.push_front(1);
+
         //1 2 3 100 200 300
+        assert_eq!(Some(&1), list.get(0));
+        assert_eq!(Some(&2), list.get(1));
+        assert_eq!(Some(&3), list.get(2));
+        assert_eq!(Some(&100), list.get(3));
+        assert_eq!(Some(&200), list.get(4));
+        assert_eq!(Some(&300), list.get(5));
+        assert_eq!(None, list.get(6));
+
         assert_eq!(Some(1), list.pop_front());
         assert_eq!(Some(2), list.pop_front());
         assert_eq!(Some(3), list.pop_front());
@@ -100,5 +154,25 @@ mod tests {
         assert_eq!(0, list.length());
         assert!(list.is_empty());
         println!("{:?}", list);
+    }
+
+    #[test]
+    fn test_box() {
+        struct A {
+            data: String,
+            data2: i32,
+        }
+
+        impl A {
+            fn test(&self) {}
+        }
+
+        let mut a = Box::new(A {
+            data: "data".to_owned(),
+            data2: 123,
+        });
+        let b = (*a).data; // 为什么这里可以move，下面一行不可以? 编译器不也是调用deref/deref_mut的么
+                           // let b = a.deref_mut().data;  //
+        a.data2;
     }
 }
