b67aefaffd878cbc6a7f07ef56bf11011327e1eb
kuiper <mzygdeaq@163.com>
2022-11-06 00:25:58 +0800

    feat: redis protocol

--- latch/src/lib.rs
+++ latch/src/lib.rs
@@ -5,6 +5,9 @@ use std::sync::Mutex;
 
 use crossbeam::utils::CachePadded;
 
+const WAITING_LIST_SHRINK_SIZE: usize = 8;
+const WAITING_LIST_MAX_CAPACITY: usize = 16;
+
 /// Lock required for a command
 #[derive(Clone)]
 pub struct Lock {
@@ -45,13 +48,70 @@ impl Lock {
 
 #[derive(Clone)]
 struct Latch {
-    // store hash value of the key and command ID which requires this key.
+    /// store hash value of the key and command ID which requires this key.
+    /// (hash, commandID)
     waiting: VecDeque<Option<(u64, u64)>>,
 }
 
 impl Latch {
     fn new() -> Latch {
-        Latch { waiting: VecDeque::new() }
+        Latch {
+            waiting: VecDeque::new(),
+        }
+    }
+
+    #[inline]
+    fn wait_for_wake(&mut self, key_hash: u64, cid: u64) {
+        self.waiting.push_back(Some((key_hash, cid)));
+    }
+
+    /// Find the first command ID in the queue whose hash value is equal to hash.
+    fn get_first_req_by_hash(&self, hash: u64) -> Option<u64> {
+        for (h, cid) in self.waiting.iter().flatten() {
+            if *h == hash {
+                return Some(*cid);
+            }
+        }
+        None
+    }
+
+    fn pop_front(&mut self, key_hash: u64) -> Option<(u64, u64)> {
+        // Maybe the first one is which we need.
+        if let Some(item) = self.waiting.pop_front() {
+            if let Some(x) = item.as_ref() {
+                if x.0 == key_hash {
+                    self.maybe_shrink();
+                    return item;
+                }
+            }
+            // If not the one we need, push it to old place.
+            self.waiting.push_front(item);
+        }
+        for it in self.waiting.iter_mut() {
+            if let Some(t) = it {
+                if t.0 == key_hash {
+                    return it.take();
+                }
+            }
+        }
+        None
+    }
+
+    /// For some hot keys, the waiting list maybe very long, so we should shrink the waiting
+    /// VecDeque after pop.
+    fn maybe_shrink(&mut self) {
+        // Pop item which is none to make queue not too long.
+        while let Some(item) = self.waiting.front() {
+            if item.is_some() {
+                break;
+            }
+            self.waiting.pop_front().unwrap();
+        }
+        if self.waiting.capacity() > WAITING_LIST_MAX_CAPACITY
+            && self.waiting.len() < WAITING_LIST_SHRINK_SIZE
+        {
+            self.waiting.shrink_to_fit();
+        }
     }
 }
 
@@ -61,9 +121,8 @@ pub struct Latches {
 }
 
 impl Latches {
-
     /// Creates latches
-    /// 
+    ///
     /// The size will be rounded up to the power of 2.
     pub fn new(size: usize) -> Latches {
         let size = size.next_power_of_two();
@@ -79,13 +138,30 @@ impl Latches {
 
 #[cfg(test)]
 mod tests {
-    use crate::Lock;
+
+    use crate::{Latch, Lock};
 
     #[test]
     fn test_lock() {
-        let mut keys = vec!["hello", "workd"];
+        let keys = vec!["hello", "world"];
         let lock = Lock::new(&keys);
+        // or let lock = Lock::new(keys);
         assert!(!lock.acquired());
-        // keys.push("valuae");
+    }
+
+    #[test]
+    fn test_latch() {
+        let mut l = Latch::new();
+        l.wait_for_wake(1024, 1);
+        l.wait_for_wake(1025, 2);
+        l.wait_for_wake(1024, 9);
+        l.wait_for_wake(1026, 3);
+        assert_eq!(l.pop_front(1024), Some((1024, 1)));
+        assert_eq!(l.pop_front(1024), Some((1024, 9)));
+        assert_eq!(l.pop_front(1026), Some((1026, 3)));
+        assert_eq!(l.pop_front(1025), Some((1025, 2)));
+        assert_eq!(l.pop_front(1024),None);
+        assert_eq!(l.pop_front(1025),None);
+        assert_eq!(l.pop_front(1026),None);
     }
 }
